1859a324037f0c79fd02156ce882152334de45e3
leone <791960492@qq.com>
2021-09-03 16:34:10 +0800

    update renew_swap.sh

--- renew_swap.sh
+++ renew_swap.sh
@@ -1,6 +1,10 @@
-sudo swapoff /swapfile
-sudo rm /swapfile
-sudo dd if=/dev/zero of=/swapfile bs=1000000000 count=20
-sudo chmod 0600 /swapfile
-sudo mkswap /swapfile
-sudo swapon /swapfile
+if [ -e "/swapfile" ];then
+	echo "remove old swapfile"
+	sudo swapoff /swapfile || exit
+	sudo rm /swapfile || exit
+fi
+echo "add new swapfile"
+sudo dd if=/dev/zero of=/swapfile bs=1000000000 count=20 || exit
+sudo chmod 0600 /swapfile || exit
+sudo mkswap /swapfile || exit
+sudo swapon /swapfile || exit
